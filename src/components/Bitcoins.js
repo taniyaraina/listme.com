32274e9db192a75c9ec2d097ccd5e2b24f2c1b9c
harjaapdhillon <harvydhillon16@gmail.com>
2020-06-20 12:34:49 +0530

    changes

---
+++ src/components/Bitcoins.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bitcoin = [
+  { image: 'client-crypto-1' },
+  { image: 'client-crypto-2' },
+  { image: 'client-crypto-3' },
+  { image: 'client-crypto-4' },
+  { image: 'client-crypto-5' },
+  { image: 'client-crypto-6' },
+];
+
+const Section = styled.div`
+  margin-top: -220rem;
+`;
+
+const Bitcoins = () => {
+  return (
+    <Section className="section">
+      <div className="columns is-gapless is-centered has-text-centered">
+        {Bitcoin.map(item => (
+          <div className="column is-2">
+            <img src={`images/${item.image}.png`} alt="bitcoin" />
+          </div>
+        ))}
+      </div>
+    </Section>
+  );
+};
+export default Bitcoins;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import HomeHero from '../components/HomeHero';
+import Bitcoin from '../components/Bitcoins';
 
 export default class IndexPage extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class IndexPage extends React.Component {
       <Layout>
         <Seo title="Home" description="Welcome to GatsbyJs v1" />
         <HomeHero />
+        <Bitcoin />
       </Layout>
     );
   }
