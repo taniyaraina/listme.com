7f4709127a2dab65c1c10d36da2da2f9796786b4
dharmveer97 <dvbangar8@gmail.com>
2020-06-28 15:48:08 +0530

    minor improvements.

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import CountUp from 'react-countup';
 import Image from '../../static/images/NumberBackground.jpg';
-
 import { theme } from '../utils/theme';
 
 const Container = styled.div`
@@ -32,22 +32,52 @@ const Numbers = () => {
           <div className="column is-10  background">
             <div className="columns is-centered is-vcentered has-text-centered">
               <div className="column is-3">
-                <h1 className="title is-3">1200</h1>
+                <h1 className="title is-3">
+                  <CountUp
+                    start={0}
+                    end={190}
+                    duration={2.75}
+                    decimals={1}
+                    decimal=","
+                  />
+                </h1>
                 <h1 className="subtitle is-5">{`< happy_clients >`}</h1>
               </div>
               <div className="column is-3">
                 {' '}
-                <h1 className="title is-3">1200</h1>
+                <h1 className="title is-3">
+                  <CountUp
+                    start={0}
+                    end={190}
+                    duration={4.75}
+                    decimals={1}
+                    decimal=","
+                  />
+                </h1>
                 <h1 className="subtitle is-5">{`< happy_clients >`}</h1>
               </div>
               <div className="column is-3">
-                {' '}
-                <h1 className="title is-3">1200</h1>
+                <h1 className="title is-3">
+                  <CountUp
+                    start={0}
+                    end={190}
+                    duration={4.75}
+                    decimals={1}
+                    decimal=","
+                  />
+                </h1>
                 <h1 className="subtitle is-5">{`< happy_clients >`}</h1>
               </div>
               <div className="column is-3">
-                {' '}
-                <h1 className="title is-3">1200</h1>
+                <h1 className="title is-3">
+                  <CountUp
+                    start={0}
+                    end={500}
+                    duration={4.75}
+                    decimals={1}
+                    decimal=","
+                  />
+                </h1>
                 <h1 className="subtitle is-5">{`< happy_clients >`}</h1>
               </div>
             </div>
