2394b1941fe5ea4c99c6a4abedfa2cb4fa28e7f0
harjaapdhillon <harvydhillon16@gmail.com>
2020-07-16 12:02:04 +0530

    changes

--- src/pages/blog-grid.js
+++ src/pages/blog-grid.js
@@ -5,16 +5,28 @@ import MapHero from '../components/MapHero';
 import Layout from '../components/Layout';
 import { theme } from '../utils/theme';
 
+import BlogGridCard from '../components/blog-gridCard';
+
 const Container = styled.div`
   min-height: 20rem;
 `;
 
-const Cart = () => {
+const Data = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+
+const BlogGrid = () => {
   return (
     <Layout>
       <MapHero Title="Blog Grid" subtitle="Blog Grid" />
-      <Container className="section container"></Container>
+      <Container className="section container">
+        <div className="columns is-multiline">
+          {Data.map(() => (
+            <div className="column is-4">
+              <BlogGridCard />
+            </div>
+          ))}
+        </div>
+      </Container>
     </Layout>
   );
 };
-export default Cart;
+export default BlogGrid;
