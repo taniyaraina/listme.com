a248687e77764d18119e9788c48ab1988d9a9466
dharmveer97 <dvbangar8@gmail.com>
2020-07-17 10:46:11 +0530

    minor improvements.

--- src/components/PaymentCard.js
+++ src/components/PaymentCard.js
@@ -19,15 +19,92 @@ const CardWrapper = styled.div`
     padding-bottom: 2rem;
     color: ${theme.hoverColor} !important;
   }
+  .services_icon_wrapper :hover {
+    animation: services_circle_wrapper 2s infinite linear;
+  }
+  @-webkit-keyframes services_circle_wrapper {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
+    }
+  }
+  @keyframes services_circle_wrapper {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
+    }
+  }
+
+  .services_icon_wrapper {
+    position: relative;
+    z-index: 1;
+    width: 110px;
+    height: 110px;
+    line-height: 110px;
+    font-size: 42px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 30px;
+    border: 1px solid CurrentColor;
+    box-sizing: content-box;
+    border-radius: 50%;
+    :before {
+      content: '';
+      z-index: -1;
+      left: 0;
+      width: 100%;
+      position: absolute;
+      height: 100%;
+      box-shadow: 0px 0px 25px 0px CurrentColor,
+        0px 0px 25px 0px CurrentColor inset;
+      border-radius: 50%;
+      opacity: 0.2;
+    }
+  }
+  .services_circle_wrapper {
+    width: 84%;
+    height: 84%;
+    left: 8%;
+    top: 8%;
+    position: absolute;
+    z-index: -1;
+  }
+  .services_circle {
+    background-color: #ca5cbc;
+    width: 14px;
+    height: 14px;
+    border-radius: 50%;
+  }
+
+  .services_circle_wrapper:nth-child(2) {
+    transform: rotate(180deg);
+    animation: service_hexagon_2 2s infinite linear;
+    animation-play-state: paused;
+  }
 `;
 
 const PaymentCard = () => {
   return (
     <CardWrapper className="card">
       <div className="card-content">
-        <figure className="mage is-32x32">
-          <img src="/images/contact/toolip-one.png" alt="icon" />
-        </figure>
+        <div className="services_icon_wrapper">
+          <div className="services_circle_wrapper">
+            <div className="services_circle" />
+          </div>
+          <div className="services_circle_wrapper">
+            <div className="services_circle" />
+          </div>
+          <i className="fas fa-home" />
+        </div>
+
         <h1 className="title is-5 has-text-weight-semibold">Any Payments</h1>
         <div className="content">
           <h1 className="subtitle is-6 has-text-grey-lighter">
