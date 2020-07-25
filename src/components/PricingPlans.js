08e99fd6805299850c2bcd52186ec5fc0e0f74aa
karanbir8872 <wwwkaranbir786@gmail.com>
2020-07-25 13:53:16 +0530

    fix the PricingPlan component

--- src/components/PricingPlans.js
+++ src/components/PricingPlans.js
@@ -20,7 +20,7 @@ const Container = styled.div`
     border: 1px solid ${theme.textColorLite};
     display: inline-block;
     border-radius: 100px;
-    padding: 0.2rem;
+    padding: 0.2rem 1.2rem 0.2rem 1.2rem;
     .button {
       border-radius: 100px;
       border-width: 0px;
@@ -31,13 +31,13 @@ const Container = styled.div`
       box-shadow: ${props =>
         props.tab ? '0px 15px 39px rgba(0, 0, 0, 0.2) !important' : ''};
       color: ${props =>
-        props.tab ? theme.darkAccent : theme.textColorLite} !important;
+        props.tab ? theme.darkAccent : theme.lightGrey} !important;
     }
     .business {
       box-shadow: ${props =>
         props.tab ? '' : '0px 15px 39px rgba(0, 0, 0, 0.2) !important'};
       color: ${props =>
-        props.tab ? theme.textColorLite : theme.darkAccent} !important;
+        props.tab ? theme.lightGrey : theme.darkAccent} !important;
     }
   }
 `;
@@ -52,24 +52,28 @@ const PricingPlans = () => {
       <section className="section">
         <h1 className="title greenColor is-5">{`< pricing_plans >`}</h1>
         <div className="columns is-centered">
-          <div className="column is-10">
-            <h1 className="title is-1 has-text-weight-bold">
-              The Best Solutions for Agencies and Business Companies
-            </h1>
+          <div className="column is-11">
+            <div className="columns is-centered">
+              <div className="column is-7">
+                <h1 className="title is-2 has-text-weight-bold is-size-4-touch">
+                  The Best Solutions for Agencies and Business Companies
+                </h1>
+              </div>
+            </div>
             <div className="buttonCollection">
               <button
                 onClick={() => TabTrue()}
-                className="button is-medium individual"
+                className="button is-medium is-size-6 individual"
                 type="button"
               >
                 Individual
               </button>
               <button
                 onClick={() => TabFalse()}
-                className="button is-medium business"
+                className="button is-medium is-size-6 business"
                 type="button"
               >
-                Business
+                Agency
               </button>
             </div>
             {tab ? <IndividualPricingPlan /> : <BusinessPricingPlan />}
