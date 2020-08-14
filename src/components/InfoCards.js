9b0994b25fa73fa0e92a99cbbaed40b21a73a78e
karanbir8872 <wwwkaranbir786@gmail.com>
2020-08-14 18:06:08 +0530

    done the infoCard

--- src/components/InfoCards.js
+++ src/components/InfoCards.js
@@ -3,6 +3,21 @@ import styled from 'styled-components';
 
 import { theme } from '../utils/theme';
 
+const InfoWrapper = [
+  {
+    title: 'Find it',
+    subtitle: 'Find where you like to go',
+  },
+  {
+    title: 'Attend it',
+    subtitle: 'Go where you found to go.',
+  },
+  {
+    title: 'Pay',
+    subtitle: 'Automatically pay to venue',
+  },
+];
+
 const Container = styled.div`
   .card {
     border-radius: 20px;
@@ -14,18 +29,19 @@ const Container = styled.div`
     }
   }
   .check {
-    background-color: ${theme.darkAccent};
+    background: radial-gradient(circle farthest-corner at 100% 110%, #68044d 10%, #9d2b7e 50%);
+    box-shadow: 0px 9px 30px 0 rgba(157,43,126,0.4)!important;
     color: ${theme.backgroundColor};
     padding: 1.4rem;
     border-radius: 100px;
     box-shadow: 0px 15px 38px rgba(0, 0, 0, 0.1);
   }
-  .arrow {
+  /* .arrow {
     transition: 0.5s;
     :hover {
       color: ${theme.darkAccent};
     }
-  }
+  } */
   .subtitle {
     margin-top: -15px !important;
   }
@@ -36,72 +52,31 @@ const InfoCards = () => {
     <section className="section">
       <div className=" container">
         <Container className="columns is-centered">
-          <div className="column is-4 margin">
-            <div className="columns card is-gapless  is-vcentered is-mobile">
-              <div className="column is-3 has-text-centered">
-                <span className="icon check">
+          {InfoWrapper.map(item => (
+            <div className="column is-4">
+              <div className="columns card is-gapless is-vcentered is-mobile">
+                <div className="column is-3 has-text-centered">
+                  <span className="icon check">
+                    {' '}
+                    <i className="fas fa-check" />
+                  </span>
+                </div>
+                <div className="column is-7">
                   {' '}
-                  <i className="fas fa-check" />
-                </span>
-              </div>
-              <div className="column is-7">
-                {' '}
-                <h1 className="title is-5">Implement Ideas</h1>
-                <h1 className="subtitle is-6  has-text-weight-normal">
-                  On your website
-                </h1>
-              </div>
-              <div className="column is-2">
-                <span className="icon is-size-4 arrow">
-                  <i className="fas fa-arrow-right" />
-                </span>
+                  <h1 className="title is-5">{item.title}</h1>
+                  <h1 className="subtitle is-6  has-text-weight-normal">
+                    {item.subtitle}
+                  </h1>
+                </div>
+                <div className="column is-2">
+                  <img src="images/arrow.png" alt="left-arrow" />
+                  {/* <span className="icon is-size-4 arrow">
+                    <i className="fas fa-arrow-right" />
+                  </span> */}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="column is-4">
-            <div className="columns card is-gapless is-vcentered is-mobile">
-              <div className="column is-3 has-text-centered">
-                <span className="icon check">
-                  {' '}
-                  <i className="fas fa-check" />
-                </span>
-              </div>
-              <div className="column is-7">
-                {' '}
-                <h1 className="title is-5">Zero Fraud</h1>
-                <h1 className="subtitle is-6  has-text-weight-normal">
-                  Gaurantee no chargebacks
-                </h1>
-              </div>
-              <div className="column is-2">
-                <span className="icon is-size-4 arrow">
-                  <i className="fas fa-arrow-right" />
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className="column is-4">
-            <div className="columns card is-gapless is-vcentered is-mobile">
-              <div className="column is-3 has-text-centered">
-                <span className="icon check">
-                  {' '}
-                  <i className="fas fa-check" />
-                </span>
-              </div>
-              <div className="column is-7">
-                {' '}
-                <h1 className="title is-5">Privacy & Security</h1>
-                <h1 className="subtitle is-6  has-text-weight-normal">
-                  Customer and merchant
-                </h1>
-              </div>
-              <div className="column is-2">
-                <span className="icon is-size-4 arrow">
-                  <i className="fas fa-arrow-right" />
-                </span>
-              </div>
-            </div>
-          </div>
+          ))}
         </Container>
       </div>
     </section>
